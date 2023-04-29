3ed94793df2ff3c07b67242a6599e2c6a5d71aa7
SabrinaStoekly <sstoekly@gmail.com>
2023-04-29 20:52:19 +0000

    Adding class BattleshipBoard, it represents the game board

--- run.py
+++ run.py
@@ -1,34 +1,57 @@
-
 # Imports
-
-import copy as c
 import os
-import random as random
+import random
+from typing import Callable
+
+#Constants used to represent the amount of guesses of each player
+#and definy the size ofthe board 5x5
+GUESSES_COUNT = 5
+BOARD_SIZE_X = 5
+BOARD_SIZE_Y = 5
+
+# Constants used to represent elements on the board
+HIDDEN = "-"
+SHIP = "S"
+GUESS = "X"
 
 
-class Game(object):
+            
+class BattleshipBoard:
     """
-    This class contains most of the data of the game
-    - creates the  board and randly creates a ship
+    This class represents the game board for the Battleship game,
+    It initializes the board and randomly places a single ship on it.
+    Checks if there is a ship at the specified row and column.
+    Checks if the specified cell has already been guessed.
+    It places a guess on the specified cell.
     """
-    def __init__(self, players):
-        self.guesses = 5
-        self.player_list = []
-        for player in range(players):
-            self.player_list.append(self.guesses)
-        self.current_player = 1
-        self.board = self.create_board(5, 5)
-        self.board_visible = c.deepcopy(self.board)
-        self.ship_row = rand.randint(0, 4)
-        self.ship_col = rand.randint(0, 4)
-        self.guess_row = 0
-        self.guess_col = 0
+    
+    def __init__(self, size_x: int, size_y: int) -> None:
+        # create the grid
+        self.grid = [[HIDDEN] * size_x for _ in range(size_y)]
+
+        # place a random ship on the grid
+        ship_row = random.randint(0, size_y - 1)
+        ship_col = random.randint(0, size_x - 1)
+        self.grid[ship_row][ship_col] = SHIP
+
+    def is_ship(self, row: int, col: int) -> bool:
+        return self.grid[row][col] == SHIP
+
+    def already_guessed(self, row: int, col: int) -> bool:
+        return self.grid[row][col] == GUESS
+
+    def place_guess(self, row: int, col: int) -> None:
+        if not self.is_ship(row, col):
+            self.grid[row][col] = GUESS
+
+    def to_string(self, show_ship: bool = False) -> str:
+        rows_str: list[str] = []
+        for row in self.grid:
+            row = [HIDDEN if col == SHIP and not show_ship else col.replace("0", HIDDEN) for col in row]
+            rows_str.append(" ".join(row))
+        return "\n".join(rows_str)
 
 
 
-
-        
-
-        
 
 
