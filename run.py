ac6049f8e506e45b3ef63676a8e3292005fa92c2
SabrinaStoekly <sstoekly@gmail.com>
2023-04-30 16:03:17 +0000

    Adding make_guess method to check the data input by user

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import random
 
 class Battleship:
     """
-    Ses up the board  and places the  ship on a random possition 
+    Keeps track of the current state of the game
+    Sets up the board  and places the  ship on a random possition 
     """
     def __init__(self):
         self.board = []
@@ -25,8 +26,34 @@ class Battleship:
         for i, row in enumerate(self.board):
             print(i+1, " ".join(row))
 
+    def make_guess(self, guess_row, guess_col):
+        """
+        Takes a row and column as arguments and 
+        checks if there is a ship at that location
+        on the board
+        """
+        self.num_turns += 1
+
+        if guess_row == self.ship_row and guess_col == self.ship_col:
+            print("Congratulations! You sank my battleship!")
+            return True
+        else:
+            if guess_row < 1 or guess_row > 5 or guess_col < 1 or guess_col > 5:
+                print("Oops, that's not even in the ocean.")
+            elif self.board[guess_row-1][guess_col-1] == "X":
+                print("You guessed that one already.")
+            else:
+                print("You missed my battleship!")
+                self.board[guess_row-1][guess_col-1] = "X"
+                self.guesses.append((guess_row, guess_col))
+
+            self.print_board()
+            return False
+
+
+
 game  = Battleship()
-print(game.print_board())
+print(game.make.guess())
 
 
 
