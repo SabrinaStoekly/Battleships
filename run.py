5476ccc20742e976118c238f6834f25509b38b40
SabrinaStoekly <sstoekly@gmail.com>
2023-04-30 15:55:32 +0000

    Adding function to print the board as a board

--- run.py
+++ run.py
@@ -14,11 +14,20 @@ class Battleship:
         for x in range(5):
             self.board.append(["-"] * 5)
 
-        
+        self.place_ship()
 
+    def place_ship(self):
+        self.ship_row = random.randint(0, 4)
+        self.ship_col = random.randint(0, 4)
+
+    def print_board(self):
+        print("  1 2 3 4 5")
+        for i, row in enumerate(self.board):
+            print(i+1, " ".join(row))
 
 game  = Battleship()
-print(game.board)
+print(game.print_board())
+
 
 
 
