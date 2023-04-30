4cb31b7c8261de8975d148f4844c9188aeb0e424
SabrinaStoekly <sstoekly@gmail.com>
2023-04-30 16:22:23 +0000

    Adding the play_game function and the welcome message with the games rules

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import random
 
+
 class Battleship:
     """
     Keeps track of the current state of the game
-    Sets up the board  and places the  ship on a random possition 
+    Sets up the board and places the ship on a random position
     """
     def __init__(self):
         self.board = []
@@ -24,11 +25,11 @@ class Battleship:
     def print_board(self):
         print("  1 2 3 4 5")
         for i, row in enumerate(self.board):
-            print(i+1, " ".join(row))
+            print(i + 1, " ".join(row))
 
     def make_guess(self, guess_row, guess_col):
         """
-        Takes a row and column as arguments and 
+        Takes a row and column as arguments and
         checks if there is a ship at that location
         on the board
         """
@@ -38,7 +39,9 @@ class Battleship:
             print("Congratulations! You sank my battleship!")
             return True
         else:
-            if guess_row < 1 or guess_row > 5 or guess_col < 1 or guess_col > 5:
+            if guess_row < 1 or guess_row > 5 or \
+              guess_col < 1 or guess_col > 5:
+                print("Oops, that's not even in the ocean.")
                 print("Oops, that's not even in the ocean.")
             elif self.board[guess_row-1][guess_col-1] == "X":
                 print("You guessed that one already.")
@@ -51,9 +54,37 @@ class Battleship:
             return False
 
 
+def play_game():
+    """
+    Prints the full and updated board to the console
+    """
+    game = Battleship()
+    print("Let's play Battleship!\n")
+    game.print_board()
 
-game  = Battleship()
-print(game.make.guess())
+    while True:
+        try:
+            guess_row = int(input("Guess Row (1-5): \n"))
+            guess_col = int(input("Guess Col (1-5): \n"))
+        except ValueError:
+            print("Oops, please enter a valid row and column number.")
+            continue
+
+        if game.make_guess(guess_row, guess_col):
+            print("You took", game.num_turns, "turns to win.")
+            print("Here are your guesses:")
+            for guess in game.guesses:
+                print(guess)
+            break
+
+
+print('--' * 25)
+print('Welcome to the BattleShip Game !!')
+print('The board size is 5x5, and it has 1 ship')
+print('You have as many guesses as you can!')
+print('--' * 25)
+play_game()
+
 
 
 
